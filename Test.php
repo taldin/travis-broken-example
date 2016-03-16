2808b4233a5aa7aca6c3209c289414702a328023
taldin <taldin@users.noreply.github.com>
2016-03-16 11:26:38 -0700

    Update Test.php

--- Test.php
+++ Test.php
@@ -3,10 +3,14 @@
 class Test extends PHPUnit_Framework_TestCase
 {
 	public function testOnePlusOne() {
-		$this->assertEquals(1+1,3);
+		$this->assertEquals(1+1,2);
   	}
   	public function testOneMinusOne() {
   		$this->assertEquals(1-1,0);
+  	}
+  	
+  	public function testa() {
+  		$this->assertEquals(test_a(1),1);
   	}
 }
 
